b223684448acb407abf0df9231db810a2acac1e9
fede <fede@192.168.2.101>
2013-04-19 16:21:23 -0300

    actualizacion grilla

--- IA02/src/TPI/Grilla.java
+++ IA02/src/TPI/Grilla.java
@@ -44,9 +44,11 @@ public class Grilla extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
             for (int row = 0; row < tmño; row++) {
                 for (int col = 0; col < tmño; col++) {
+                    // se inicializa la matriz de estados en 0 = normal;
                     grilla[col][row]=0;
                     gbc.gridx = col;
                     gbc.gridy = row;
+                    // creamos la celda nueva y le damos un borde segun posicion
                     Posicion pos = new Posicion(col,row);
                     Celda celda = new Celda(pos);
                     Border border;
@@ -65,10 +67,26 @@ public class Grilla extends JPanel {
                     }
                     celda.setBorder(border);
                     celda.setBackground(Color.LIGHT_GRAY);
+                    // añadimos la celda en la grilla con el layout y en la lista de las celdas
                     add(celda, gbc);
                     matrizCeldas [col][row]=celda;
                 }
             }
     }
+    //metodo para crear la grilla de estados
+    public void armarGrilla(){
+        for (int i=0;i<tmño;i++){
+            for(int j=0;i<tmño;j++){
+                int tipo = matrizCeldas[i][j].getTipo();
+                grilla[i][j]=tipo;
+            }
+        }
+    }
+    //getter de la grilla
+    public int[][] getGrilla() {
+        return grilla;
+    }
+    
+    
     
 }
