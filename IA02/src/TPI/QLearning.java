936d8f8bc24095e999c581bf3ac942bfd8f46c34
fede <fede@192.168.2.101>
2013-04-19 17:45:11 -0300

    añadidas funciones de seleccion de accion

--- IA02/src/TPI/QLearning.java
+++ IA02/src/TPI/QLearning.java
@@ -39,9 +39,10 @@ public class QLearning {
     int map[][];
     //tamaño de la columna/fila
     int tamaño=0;
-    
+    // a modo de prueba por ahora
+    Celda matrizCelda[][];
     //constructor
-    public QLearning (int tmño,long itmax, double exp,double apren, double temp, double amort, double recB, double recE, double recN, double recF,double recM,int [][] mapa){
+    public QLearning (int tmño,long itmax, double exp,double apren, double temp, double amort, double recB, double recE, double recN, double recF,double recM,int [][] mapa,Celda [][] mapCell){
         this.maxIteracion=itmax;
         this.tamaño=tmño;
         this.Tau=temp;
@@ -53,6 +54,7 @@ public class QLearning {
         this.recFinal=recF;
         this.recNormal=recN;
         this.map=mapa;
+        this.matrizCelda=mapCell;
         //iniciar la tabla de q, el 8 va por las 8 acciones posibles 
         Qvalues=new double[tamaño][tamaño][8];
         for(int i=0;i<tamaño;i++){
@@ -63,6 +65,37 @@ public class QLearning {
             }
         }
     }
-    
+    //egreedy
+    public int eGreedy(Posicion pos){
+        int accion;
+        int i=pos.getI(); int j=pos.getJ();
+        Celda celda = matrizCelda[i][j];
+        double random = java.lang.Math.random();
+        if (random<this.epsilon){
+            //cae dentro de la exploracion, accion es aleatoria
+            int indice = java.lang.Math.round((float)java.lang.Math.random()*(celda.getCantAccion()));
+            accion = (int) celda.listaAcciones.get(indice);
+        } else {
+            //cae dentro de la parte de explotacion, accion es la mejor
+            accion = mejorAccion(pos);
+        }
+        return accion;
+    }
+    //para egreedy mejor accion, es greedy, devuelve la accion que da el mejor Q
+    public int mejorAccion(Posicion pos){
+        int laMejor = 0;
+        double mejorQ=-10000.0;
+        int i=pos.getI(); int j=pos.getJ();
+        Celda celda = matrizCelda[i][j];
+        //entre las acciones posibles, miro la que devuelve el mejorQ
+        for(int x=0;x<=celda.getCantAccion();x++){
+            int a = (int) celda.listaAcciones.get(x);
+            if(Qvalues[i][j][a]>mejorQ){
+                mejorQ=Qvalues[i][j][a];
+                laMejor=a;
+            }            
+        }
+        return laMejor;
+    }
     
 }
